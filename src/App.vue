565e9f48dbb08ae6027c990b02eebef9755a27ff
danie <danie@DESKTOP-K284EKP>
2023-11-06 15:06:10 +0800

    gitignore

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <main class="main">
+    <h1>test</h1>
+  </main>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+.main {
+  background-image: url('../src/assets/bg.jpg');
+  background-size: cover;
+}
+</style>
